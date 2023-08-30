f9092474ba78a7002003b033f320e09108e56dbd
Scorppb <Scorppb@yandex.ru>
2023-08-30 20:38:49 +0300

    dz 2 task 2

---
+++ 02/01.py
@@ -0,0 +1,41 @@
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление. 
+# Функцию hex используйте для проверки своего результата.
+
+
+def input_numbers(inputText):
+    check_input = False
+    while not check_input:
+        try:
+            number = int(input(f'{inputText}'))
+            check_input = True
+        except ValueError:
+            print('Вы вводите не цифры, введите корректные данные')
+    return number
+
+
+def to_hex(number):
+    hex_string = ""
+    while number > 0:
+        match number%16:
+            case 10:
+                hex_string = "a" + hex_string
+            case 11:
+                hex_string = "b" + hex_string
+            case 12:
+                hex_string = "c" + hex_string
+            case 13:
+                hex_string = "d" + hex_string
+            case 14:
+                hex_string = "e" + hex_string
+            case 15:
+                hex_string = "f" + hex_string
+            case _:
+                hex_string = str(number%16) + hex_string
+        number //= 16
+    return "0x" + hex_string
+
+
+
+number = input_numbers('Введите целое число> ')
+print(f"Шестнадцатеричное значение целого {number } равно {to_hex(number)}")
+print(f"Проверка {hex(number)}")
---
+++ 02/02.py
@@ -0,0 +1,55 @@
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
+
+
+from fractions import Fraction
+
+def gcd(a, b): 
+    if a > b:   
+        temp = b 
+    else: 
+        temp = a 
+    for i in range(1, temp + 1): 
+        if(( a % i == 0) and(b % i == 0 )): 
+            gcd = i 
+    return gcd 
+
+
+def summa(string1, string2):
+    a1, b1 = str(string1).split(sep="/")
+    a2, b2 = str(string2).split(sep="/")
+    a = int(a1)*int(b2) + int(a2)* int(b1)
+    b = int(b1) * int(b2)
+    divider = gcd(a, b)
+    if divider == 1 and b !=1:
+        return f"{a}/{b}"
+    elif a == b:
+        return "1"
+    elif b == 1:
+        return a
+    else:
+        return f"{a/divider:.0f}/{b/divider:.0f}"
+
+
+def multiply(string, string2):
+    a1, b1 = str(string1).split(sep="/")
+    a2, b2 = str(string2).split(sep="/")
+    a = int(a1)*int(a2)
+    b = int(b1) * int(b2)
+    divider = gcd(a, b)
+    if divider == 1 and b !=1:
+        return f"{a}/{b}"
+    elif a == b:
+        return "1"
+    elif b == 1:
+        return a
+    else:
+        return f"{a/divider:.0f}/{b/divider:.0f}"
+
+string1 = input("Введите дробь вида а/b: ")
+string2 = input("Введите дробь вида c/d: ")
+
+print(f"Сумма дробей {string1} и {string2} равна {summa(string1, string2)}")
+print(f"Проверка {Fraction(string1) + Fraction(string2)}")
+print(f"Произведение дробей {string1} и {string2} равно {multiply(string1, string2)}")
+print(f"Проверка {Fraction(string1) * Fraction(string2)}")
