18a61f047ec33e5f39b6eefdb13b29d9fcb69131
Scorppb <Scorppb@yandex.ru>
2023-08-31 17:43:42 +0300

    dz 3 tesk 1

--- 03/01.py
+++ 03/01.py
@@ -2,3 +2,18 @@
 # В результирующем списке не должно быть дубликатов.
 
 
+def remove_duplicates(source_list):
+
+    my_set = set()
+
+    for item in source_list:
+        if source_list.count(item) > 0:
+            my_set.add(item)
+
+    return list(my_set)
+
+
+source_list = [1, 3, 3, 2, 5, 7, 2, 6, 9, 5, 6, 1, 2, 2, 4]
+print(f"Исходный список {source_list}")
+print(f"Список без дубликатов {remove_duplicates(source_list)}")
+
